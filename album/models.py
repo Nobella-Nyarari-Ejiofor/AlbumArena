bd58371682cfddf2501477947e2ff1ff09b73c48
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-02-26 22:14:00 -0500

    Ran chechecks on the models and got 0 errors

--- album/models.py
+++ album/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 # The image model 
-class Location('models.Model'):
+class Location(models.Model):
   location = models.CharField(max_length = 20)
   
   def __str__(self):
@@ -21,8 +21,8 @@ class Image(models.Model):
   image = models.ImageField(upload_to ='album/')
   image_name = models.CharField(max_length= 30)
   image_description=models.CharField(max_length = 300)
-  category = models.ForeignKey(Category)
-  location = models.ForeignKey(Location)
+  category = models.ForeignKey(Category, on_delete= models.CASCADE)
+  location = models.ForeignKey(Location, on_delete= models.CASCADE)
 
   def __str__(self):
     return self.image_name
