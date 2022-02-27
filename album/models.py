619c240ca871eb93c4ca832e5c1a8647be7b4082
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-02-26 23:11:52 -0500

    Created tests for save functionality

--- album/models.py
+++ album/models.py
@@ -3,13 +3,18 @@ from django.db import models
 # Create your models here.
 # The image model 
 class Location(models.Model):
+  id = models.CharField(max_length = 100 , primary_key = True)
   location_name = models.CharField(max_length = 20)
   
   def __str__(self):
     return self.location_name
 
+  def save_locations(self):
+    self.save()
+
 
 class Category(models.Model):
+  id = models.CharField(max_length = 100 , primary_key = True)
   category_name = models.CharField(max_length = 10)
   
   def __str__(self):
@@ -18,6 +23,10 @@ class Category(models.Model):
   # saving a category
   def save_category(self):
     self.save()
+
+  #deleting a category
+  def delete_category(self):
+    self.delete()
 
 
 
--- album/tests.py
+++ album/tests.py
@@ -19,3 +19,25 @@ class CategoryTestClass(TestCase):
     self.food.save_category()
     categories = Category.objects.all()
     self.assertTrue(len(categories)>0)
+
+  # test for deleting functionality
+  def test_delete_method(self):
+    d = self.food.id
+    self.food.delete_category()
+    categories = Category.objects.get(id__exact=d)
+    self.assertEqual(categories,(None))
+
+class LocationTestClass(TestCase):
+  def setUp(self):
+    self.Nairobi = Location(location_name = "Nairobi")
+
+  # testing instance
+  def test_instance(self):
+    self.assertTrue(isinstance(self.Nairobi , Location))
+  
+  #testing the save method
+
+  def test_save_method(self):
+    self.Nairobi.save_locations()
+    locations = Location.objects.all()
+    self.assertTrue(len(locations)>0)
