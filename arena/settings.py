22d60d0612bc87af408255fc5ed2d9c2f96f83f6
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-03-01 19:54:19 -0500

    deploying to heroku

--- arena/settings.py
+++ arena/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-drtqbxlhsm2uf)83k5f$$p*314)r69eoeohyn@qe%@a)s8r3rn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://albumarena.com',
+ALLOWED_HOSTS = ['https://albumarena.herokuapp.com/',
 'localhost',
 '127.0.0.1']
 
